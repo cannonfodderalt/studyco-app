6133480f657657cc313c75620c9ee23250c0e2bf
cannonfodderalt <cannonfodderalt@gmail.com>
2025-08-19 18:30:11 -0700

    remove front and backend to prepare  for submodules

--- backend/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# Register your models here.
-admin.site.register(models.StudySpot)
-admin.site.register(models.Criteria)
-admin.site.register(models.SpotCriteria)
